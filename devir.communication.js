e8527abb6fe7b58bba39b5e597e9b4d164fc1934
alibek-karimov <alibek.karimov@gmail.com>
2015-01-27 16:17:47 +0600

    rooms

--- devir.communication.js
+++ devir.communication.js
@@ -2,6 +2,8 @@ var http = require('http');
 var socketio = require('socket.io');
 var express = require('express');
 var app = express();
+
+
 app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -13,16 +15,27 @@ app.use(function(req, res, next) {
 var server = http.createServer(app).listen(6888);
 var io = socketio.listen(server, {log:false, origins:'*:*'});
 console.log('socket ready');
+var subsribers = [];
+
 
 io.sockets.on("connection", function(socket) {
 	console.log('connected');
+
 	socket.on('message', function(data) {
 		var jsonData = JSON.parse(data);
 		console.log("message: " + jsonData.EventName);
-		console.log('data: ' + jsonData.EventData);
-		socket.broadcast.emit(jsonData.EventName , jsonData.EventData);
+		
+		socket.broadcast.to(jsonData.EventName).emit(jsonData.EventName , jsonData.EventData);
 	});
+
+	socket.on('subscribe', function(data) {
+		socket.join(data.EventName);
+	});
+
+
 });
+
+
 
 
 
