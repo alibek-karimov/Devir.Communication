2e6eb332452a249d7dd59e53aa5acae5b019833a
Ivan <SetInNirvana@gmail.com>
2015-04-29 14:13:32 +0600

    With unsubscribe

--- devir.communication.js
+++ devir.communication.js
@@ -1,38 +1,48 @@
 var http = require('http');
 var socketio = require('socket.io');
+var net = require("net");
 var express = require('express');
 var app = express();
 
-app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-        next();
-    });
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    next();
+});
 
 var server = http.createServer(app).listen(6888);
-var io = socketio.listen(server, {log:false, origins:'*:*'});
+var io = socketio.listen(server, { log: false, origins: '*:*' });
 console.log('socket ready');
 
+function Now() {
+    var date = new Date();
+    return '[' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + '] ';
+}
 
-io.sockets.on("connection", function(socket) {
-	console.log('connected');
+io.sockets.on('connection', function (socket) {
+    console.log('connected');
 
-	socket.on('message', function(data) {
-		if (typeof data != "object")
-			data = JSON.parse(data);
-		console.log("message: " + data.EventName);
-		
-		socket.broadcast.to(data.EventName).emit(data.EventName , data.EventData);
-	});
+    socket.on('message', function (data) {
+        if (typeof data != 'object')
+            data = JSON.parse(data);
+        console.log(Now() + 'message: ' + data.EventName);
+        socket.to(data.EventName).emit(data.EventName, data.EventData);
+    });
 
-	socket.on('subscribe', function(data) {
-		
-		if (typeof data != "object")
-			data = JSON.parse(data);
-		console.log('subscribe' + data.EventName);
-		socket.join(data.EventName);
-	});
+    socket.on('subscribe', function (data) {
+        if (typeof data != 'object')
+            data = JSON.parse(data);
+        console.log(Now() + 'subscribe: ' + data.EventName);
+        socket.join(data.EventName);
+    });
+
+    socket.on('[' + new Date() + ']' + 'unsubscribe', function (data) {
+        if (typeof data != 'object')
+            data = JSON.parse(data);
+        console.log(Now() + 'unsubscribe: ' + data.EventName)
+        socket.leave(data.EventName);
+    });
 
 });
